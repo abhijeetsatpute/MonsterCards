1319b89a466194bb8a5c10735491b21572726b5e
Abhijeet Satpute <abhi111sat@gmail.com>
2021-12-18 22:13:32 +0530

    Updates

---
+++ precache-manifest.8d47b660565de5a45adec9639bf0f5d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a378747b2137f1a6f0f",
+    "url": "/MonsterCards/static/css/main.93f2d6d9.chunk.css"
+  },
+  {
+    "revision": "4a378747b2137f1a6f0f",
+    "url": "/MonsterCards/static/js/main.13f201e4.chunk.js"
+  },
+  {
+    "revision": "c04f4fe6b8a134d06cb7",
+    "url": "/MonsterCards/static/js/runtime~main.32f05da1.js"
+  },
+  {
+    "revision": "04672c45b92fb624cccd",
+    "url": "/MonsterCards/static/css/2.dec1bece.chunk.css"
+  },
+  {
+    "revision": "04672c45b92fb624cccd",
+    "url": "/MonsterCards/static/js/2.f2c8162a.chunk.js"
+  },
+  {
+    "revision": "510535223dcd279f3967",
+    "url": "/MonsterCards/static/js/3.e9d442d7.chunk.js"
+  },
+  {
+    "revision": "3c99832cbf42474a4bccdc272767b559",
+    "url": "/MonsterCards/static/media/Thorn-4Bn19.3c99832c.ttf"
+  },
+  {
+    "revision": "69ef009d4b963311f9e22441acc1d392",
+    "url": "/MonsterCards/index.html"
+  }
+];
